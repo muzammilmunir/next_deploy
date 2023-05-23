64648b9f863dee62d927e4f317fcf9db626988ab
autodashboard <113629900+autodashboard@users.noreply.github.com>
2023-05-23 16:15:45 +0500

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import '@styles/style-static-page-1.css';
-import Head from 'next/head';
+// import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Script from 'next/script';
@@ -112,7 +112,7 @@ export const stackAnimation = ()=>{
         opacity: 0,
         scrub: 2,
     });
-    timeln.addLabel('card3');
+    timeln.addLabel('card4');
     
     timeln.to(".cardStack-1", {
         scale: 0.85,
@@ -147,13 +147,65 @@ export const stackAnimation = ()=>{
     });
 }
 
+export const flyCardAnimation = ()=>{
+    let timeln = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".flyCardStacks",
+            pin: true,
+            pinSpacing: true,
+            start: "left-=20px left",
+            end: "+=2000",
+            scrub: true
+        }
+    });
+    
+    timeln.addLabel('flycard1');
+    timeln.from('.flyCard-1', {
+        yPercent: 0,
+        opacity: 1
+    });
+    
+    timeln.addLabel("flycard2");
+    
+    timeln.to(".flyCard-1", {
+        yPercent: -150,
+        rotation: 180
+    });
+    
+    timeln.from('.flyCard-2', {
+        yPercent: 0,
+        opacity: 1
+    });
+    
+    timeln.addLabel('flycard3');
+    
+    timeln.to(".flyCard-2", {
+        yPercent: -150,
+        rotation: 180
+    });
+    
+    timeln.from(".flyCard-3", {
+        yPercent: 0,
+        opacity: 1,
+    });
+    
+    timeln.addLabel('flycard4');
+    
+    timeln.to(".flyCard-3", {
+        yPercent: -150,
+        rotation: 180
+    });
+}
+
 
 const page = () => {
 
     const main = useRef();
+    const main2 = useRef();
 
     useEffect(()=>{
         stackAnimation();
+        flyCardAnimation();
     },[])
 
     // useEffect(() => {
@@ -358,16 +410,33 @@ const page = () => {
     </div>
 
     {/* <!-- Our Work--> */}
-    <div className="px-48 my-48 relative">
-        <div className="flex items-center">
+    <div className="px-48 my-48 relative flyCardStacks" ref={main}>
+        <div className="flex items-center justify-between">
             <div>
                 <p className="text-[#171A46] text-[180px] font-black">Our</p>
             </div>
             <div>
-                <Image src="/assets/images/work-image1.png" className='relative top-0' width={698} height={797}  alt=""/>
-                <Image src="/assets/images/work-image2.png" className='absolute top-0 -z-10' width={698} height={797}  alt=""/>
-                <Image src="/assets/images/work-image3.png" className='absolute top-0 -z-20' width={698} height={797}  alt=""/>
-                <Image src="/assets/images/work-image4.png" className='absolute top-0 -z-30' width={698} height={797}  alt=""/>
+                <div className='w-[565px] h-[693px] relative top-0 bg-[#171A44] rounded-[43px] z-0 p-[32px] flyCard-1'>
+                    <p className='text-[37px] text-white font-black mb-[32px]'>Westpoint Pakistan</p>
+                    <Image src="/assets/images/westpoint-image.png" width={487} height={326}/>
+                    <p className='text-[16px] text-white mt-[48px]'>Choose Expobird as your digital marketing agency and propel your business to new heights</p>
+                    <p className='text-[48px] text-white font-semibold'>+300%</p>
+                    <div class="flex justify-end">
+                        <Image src="/assets/images/arrow-black.png" width={28} height={28} className='invert brightness-0'/>
+                    </div>
+                </div>
+                <div className='w-[565px] h-[693px] absolute top-0 bg-[#5D64CB] rounded-[43px] -z-10 -rotate-[4deg] p-[32px] flyCard-2'>
+                    <Image src="/assets/images/westpoint-image2.png" width={280} height={240}/>
+                    <p className='text-[60px] font-semibold text-[#391952] leading-none mt-[32px]'>Set up your Swag Spend Account and instantly set up your digital Visa Debit card.</p>
+                </div>
+                <div className='w-[565px] h-[693px] absolute top-0 bg-[#9399E9] rounded-[43px] -z-20 -rotate-[8deg] p-[32px] flyCard-3'>
+                    <Image src="/assets/images/westpoint-image3.png" width={280} height={240}/>
+                    <p className='text-[60px] font-semibold text-[#391952] leading-none mt-[32px]'>Spend and save on Cashback offers and in the Swag store.</p>
+                </div>
+                <div className='w-[565px] h-[693px] absolute top-0 bg-[#E3E5FF] rounded-[43px] -z-30 -rotate-[12deg] p-[32px] flyCard-4'>
+                    <Image src="/assets/images/westpoint-image4.png" width={280} height={240}/>
+                    <p className='text-[60px] font-semibold text-[#391952] leading-none mt-[32px]'>Receive your physical Swag Visa Debit card in the mail after 5-7 days.</p>
+                </div>
             </div>
             <div>
                 <p className="text-[#171A46] text-[180px] font-black">Work</p>
